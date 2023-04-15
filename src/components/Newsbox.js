31379a45ee33c7f1f9b65ef8473676d4e35aa48d
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-04-15 11:10:01 +0530

    adding structure

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -5,10 +5,33 @@ import Newsitems from './Newsitems'
 export default class Newsbox extends Component {
   render() {
     return (
-        <div> box
-            <Newsitems/>
-        
+      <div className="container">
+        <h2 className="my-3 ">Highlights</h2>
+        <div className="container my-4">
+          <div className="row">
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+            <div className="col-md-4">
+              <Newsitems />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
