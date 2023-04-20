7d834408e90ad9cf518b99ddbc0bbae1d778d79f
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-04-20 16:10:46 +0200

    --

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Spinner from "./Spinner";
 import Newsitems from "./Newsitems";
 
 export default class Newsbox extends Component {
@@ -9,39 +9,47 @@ export default class Newsbox extends Component {
       articles: [],
     };
   }
+
   async componentDidMount() {
     let fetchingurl =
-      "https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&page=1";
+      "https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&pageSize=20&page=1";
     const response = await fetch(fetchingurl);
     const data = await response.json();
 
     this.setState({
       articles: data.articles,
       page: 1,
+      totalResults: data.totalResults,
+      status: data.status,
+      loading: false,
     });
   }
 
   prevpage = async () => {
-    let fetchingurl = `https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&page=${
+    let fetchingurl = `https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&pageSize=20&page=${
       this.state.page - 1
     }`;
+    this.setState({ loading: true });
     let response = await fetch(fetchingurl);
     let data = await response.json();
     this.setState({
       articles: data.articles,
       page: this.state.page - 1,
+      loading: false,
     });
   };
 
   nextpage = async () => {
-    let fetchingurl = `https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&page=${
+    let fetchingurl = `https://newsapi.org/v2/everything?q=India&apiKey=2df9f61b4103445cb7786b27d42d1265&pageSize=20&page=${
       this.state.page + 1
     }`;
+    this.setState({ loading: true });
     let response = await fetch(fetchingurl);
     let data = await response.json();
     this.setState({
       articles: data.articles,
       page: this.state.page + 1,
+      loading: false,
     });
   };
 
@@ -51,26 +59,27 @@ export default class Newsbox extends Component {
         <h2 className="my-3 ">Highlights</h2>
         <div className="container my-4">
           <div className="row">
-            {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <Newsitems
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 90)
-                        : ""
-                    }
-                    imgurl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://1.semantic-ui.com/images/wireframe/square-image.png"
-                    }
-                    newsurl={element.url}
-                  />
-                </div>
-              );
-            })}
+            {this.state.articles &&
+              this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <Newsitems
+                      title={element.title ? element.title.slice(0, 40) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 90)
+                          : ""
+                      }
+                      imgurl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://1.semantic-ui.com/images/wireframe/square-image.png"
+                      }
+                      newsurl={element.url}
+                    />
+                  </div>
+                );
+              })}
           </div>
           <div className="d-flex justify-content-between">
             <button
@@ -82,10 +91,19 @@ export default class Newsbox extends Component {
               &larr; Previous
             </button>
             <button
+              disabled={this.state.status === "error"}
               type="button"
               className="btn btn-dark"
               onClick={this.nextpage}
             >
+              {console.log(" page " + this.state.page)}
+              {console.log(" status " + this.state.status)}
+              {console.log(" tr " + this.state.totalResults)}
+              {console.log(" ceil " + Math.ceil(this.state.totalResults / 20))}
+              {console.log(
+                " dis " + this.state.page >=
+                  Math.ceil(this.state.totalResults / 20)
+              )}
               Next &rarr;
             </button>
           </div>
