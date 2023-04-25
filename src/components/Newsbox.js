8bc7efc679be5143a6f2bd8caa75c273352c327d
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-04-25 21:06:35 +0530

    minor changes

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Spinner from "./Spinner";
 import Newsitems from "./Newsitems";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class Newsbox extends Component {
   static propTypes = {
@@ -46,6 +47,15 @@ export default class Newsbox extends Component {
     }
   }
 
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 }, async () => {
+      const newArticles = await this.fetchNews();
+      this.setState({
+        articles: [...this.state.articles, ...newArticles],
+      });
+    });
+  };
+
   fetchNews = async () => {
     this.setState({ loading: true });
     let fetchingurl = `https://newsapi.org/v2/top-headlines?country=in&category=${
@@ -57,20 +67,17 @@ export default class Newsbox extends Component {
     const data = await response.json();
 
     this.setState({
-      articles: data.articles,
+      articles:
+        this.state.page === 1
+          ? data.articles
+          : [...this.state.articles, ...data.articles],
       totalResults: data.totalResults,
       status: data.status,
       loading: false,
     });
+    return data.articles;
   };
 
-  prevpage = () => {
-    this.setState({ page: this.state.page - 1 });
-  };
-
-  nextpage = () => {
-    this.setState({ page: this.state.page + 1 });
-  };
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
@@ -82,54 +89,40 @@ export default class Newsbox extends Component {
           Headlines - {this.capitalizeFirstLetter(this.props.category)}
         </h2>
         <div className="container my-4">
-          {this.state.loading && <Spinner />}
-          <div className="row">
-            {!this.state.loading &&
-              this.state.articles &&
-              this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <Newsitems
-                      title={element.title ? element.title.slice(0, 40) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 90)
-                          : ""
-                      }
-                      imgurl={
-                        element.urlToImage
-                          ? element.urlToImage
-                          : "https://1.semantic-ui.com/images/wireframe/square-image.png"
-                      }
-                      newsurl={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                    />
-                  </div>
-                );
-              })}
-          </div>
-          <div className="d-flex justify-content-between">
-            <button
-              disabled={this.state.page <= 1}
-              type="button"
-              className="btn btn-dark"
-              onClick={this.prevpage}
-            >
-              &larr; Previous
-            </button>
-            <button
-              disabled={
-                this.state.status === "error" ||
-                this.state.page >= Math.ceil(this.state.totalResults / 30)
-              }
-              type="button"
-              className="btn btn-dark"
-              onClick={this.nextpage}
-            >
-              Next &rarr;
-            </button>
-          </div>
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length < this.state.totalResults}
+            loader={<Spinner />}
+          >
+            <div className="row">
+              {!this.state.loading &&
+                this.state.articles &&
+                this.state.articles.map((element, index) => {
+                  return (
+                    <div className="col-md-4" key={element.url || index}>
+                      <Newsitems
+                        title={element.title ? element.title.slice(0, 40) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 90)
+                            : ""
+                        }
+                        imgurl={
+                          element.urlToImage
+                            ? element.urlToImage
+                            : "https://1.semantic-ui.com/images/wireframe/square-image.png"
+                        }
+                        newsurl={element.url}
+                        author={element.author}
+                        date={element.publishedAt}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
+          </InfiniteScroll>
+          <div className="d-flex justify-content-between"></div>
         </div>
       </div>
     );
