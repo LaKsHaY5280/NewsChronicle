a6c1d703286e12b4b53af88b03905b1f77805f9a
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-04-30 14:54:46 +0530

    started changing class based to function based

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -6,164 +6,105 @@ import "./Newsbosty.css";
 import noimg from "./noimg.jpg";
 import Newsitems from "./Newsitems.js";
 
-export default class Newsbox extends Component {
-  static propTypes = {
-    category: PropTypes.string,
-  };
-  static defaultProps = {
-    category: "general",
-    defaultkey: "default",
-  };
+const Newsbox = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      newArticles: [],
-      page: 1,
-      totalResults: 0,
-      status: "",
-      loading: false,
-    };
-    document.title = `Newschronicle -  ${this.capitalizeFirstLetter(
-      this.props.category
-    )}`;
-  }
-
-  updateDocumentTitle = () => {
-    document.title = `Newschronicle - ${this.capitalizeFirstLetter(
-      this.props.category
-    )}`;
-  };
-
-  async componentDidMount() {
-    this.fetchNews();
-    this.updateDocumentTitle();
-  }
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.category !== this.props.category) {
-      this.setState(
-        {
-          articles: [],
-          page: 1,
-        },
-        async () => {
-          this.fetchNews();
-          this.updateDocumentTitle();
-        }
-      );
-    } else if (prevState.page !== this.state.page) {
-      this.fetchNews();
-      this.updateDocumentTitle();
-    }
-  }
-
-  fetchMoreData = async () => {
-    this.setState(
-      {
-        page: this.state.page + 1,
-        loading: true,
-      },
-      async () => {
-        const newArticles = await this.fetchNews();
-        this.setState({
-          articles: [...this.state.articles, ...newArticles],
-          loading: false,
-        });
-      }
-    );
-  };
-
-  fetchNews = async () => {
-    this.props.setprog(10);
-    this.setState({ loading: true });
-    const fetchingurl = `https://newsapi.org/v2/top-headlines?&apiKey=${this.props.newskey}&country=in&pageSize=6&page=${this.state.page}&category=${this.props.category}`;
-
-  
-    try {
-      const response = await fetch(fetchingurl);
-      const data = await response.json();
-      const articleUrls = new Set(this.state.articles.map((article) => article.url));
-      const uniqueArticles = data.articles.filter((article) => !articleUrls.has(article.url));
-      this.setState({
-        articles: [...this.state.articles, ...uniqueArticles],
-        totalResults: data.totalResults,
-      });
-      this.props.setprog(100);
-      return uniqueArticles ;
-    } catch (error) {
-      console.log(error);
-      this.setState({ loading: false, status: "error" });
-    }
-  };
-  
-
-  capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  render() {
-    return (
-      <div className="container my-5">
-        <h1 className="text-center my-5">
-          Newschronicle - {this.capitalizeFirstLetter(this.props.category)}
-        </h1>
+  const updateNews = async () => {
+    props.setprog(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.newskey}&page=${page}&pageSize=6`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setprog(30);
+    let parsedData = await data.json();
+    props.setprog(70);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setprog(100);
+  };
 
-        <InfiniteScroll
-          dataLength={this.state.articles ? this.state.articles.length : 0}
-          style={{ overflow: "hidden" }}
-          next={this.fetchMoreData}
-          hasMore={
-            this.state.articles &&
-            this.state.articles.length &&
-            this.state.articles.length < this.state.totalResults
-          }
-          loader={<Spinner />}
-          endMessage={
-            <p className="text-center">
-              {this.state.status === "error"
-                ? "Oops! Something went wrong."
-                : "Yay! You have seen it all."}
-            </p>
-          }
-        >
-          <div className="row">
-            {!this.state.articles ||
-            (this.state.articles.length === 0 && !this.state.loading) ? (
-              <div className="alert alert-danger" role="alert">
-                No news articles found.
-              </div>
-            ) : (
-              this.state.articles.map((element) => {
-                if (element) {
-                  return (
-                    <div key={element.url} className="col-md-4">
-                      <Newsitems
-                        title={element.title ? element.title : "No title"}
-                        description={
-                          element.description
-                            ? element.description
-                            : "No description"
-                        }
-                        imgurl={element.urlToImage ? element.urlToImage : noimg}
-                        newsurl={element.url}
-                        author={element.author ? element.author : "Unknown"}
-                        date={
-                          element.publishedAt ? element.publishedAt : "unknown"
-                        }
-                      />
-                    </div>
-                  );
-                } else {
-                  return null;
-                }
-              })
-            )}
-          </div>
-            <div></div>
-        </InfiniteScroll>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    updateNews();
+  }, [props.category]);
+
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.newskey}&page=${page}&pageSize=6`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+  };
+
+  return (
+    <div className="container my-5">
+      <h1 className="text-center my-5">
+        Newschronicle - {capitalizeFirstLetter(props.category)}
+      </h1>
+
+      <InfiniteScroll
+        dataLength={articles ? articles.length : 0}
+        style={{ overflow: "hidden" }}
+        next={fetchMoreData}
+        hasMore={articles && articles.length && articles.length < totalResults}
+        loader={<Spinner />}
+        endMessage={
+          <p className="text-center">
+            {status === "error"
+              ? "Oops! Something went wrong."
+              : "Yay! You have seen it all."}
+          </p>
+        }
+      >
+        <div className="row">
+          {!articles || (articles.length === 0 && !loading) ? (
+            <div className="alert alert-danger" role="alert">
+              No news articles found.
+            </div>
+          ) : (
+            articles.map((element) => {
+              if (element) {
+                return (
+                  <div key={element.url} className="col-md-4">
+                    <Newsitems
+                      title={element.title ? element.title : "No title"}
+                      description={
+                        element.description
+                          ? element.description
+                          : "No description"
+                      }
+                      imgurl={element.urlToImage ? element.urlToImage : noimg}
+                      newsurl={element.url}
+                      author={element.author ? element.author : "Unknown"}
+                      date={
+                        element.publishedAt ? element.publishedAt : "unknown"
+                      }
+                    />
+                  </div>
+                );
+              }
+            })
+          )}
+        </div>
+        <div></div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+Newsbox.propTypes = {
+  category: PropTypes.string,
+};
+Newsbox.defaultProps = {
+  category: "general",
+  defaultkey: "default",
+};
+
+export default Newsbox;
--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -1,33 +1,31 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import React, { Component } from "react";
+import React from "react";
 
-export default class Newsitems extends Component {
-  render() {
-    let { title, description, imgurl, newsurl, author, date } = this.props;
+const Newsitems = (props) => {
+  let { title, description, imgurl, newsurl, author, date } = props;
 
-    return (
-      <div>
-        <div className="card my-3">
-          <img
-            src={imgurl}
-            className="card-img-top"
-            alt="Image could not be found"
-            style={{ height: "200px", width: " 100% " }}
-          />
-          <div className="card-body ">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author ? author : "Unknown"} on {new Date(date).toGMTString() }
-              </small>
-            </p>
-            <a href={newsurl} className="btn btn-sm btn-primary">
-              Read more
-            </a>
-          </div>
+  return (
+    <div>
+      <div className="card my-3">
+        <img
+          src={imgurl}
+          className="card-img-top"
+          alt="It could not be found"
+          style={{ height: "200px", width: " 100% " }}
+        />
+        <div className="card-body ">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a href={newsurl} className="btn btn-sm btn-primary">
+            Read more
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default Newsitems;
